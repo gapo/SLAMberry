220854d1a8007ece493201f4efe0fdd90d453c2a
Gapo <gapotheflo@gmail.com>
2016-04-04 18:11:06 -0400

    Add ORBSLAM2 directory.

---
+++ deps/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gapo/meng/deps/ORB_SLAM2/src/Converter.cc" "/home/gapo/meng/deps/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Converter.cc.o"
+  "/home/gapo/meng/deps/ORB_SLAM2/src/Frame.cc" "/home/gapo/meng/deps/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Frame.cc.o"
+  "/home/gapo/meng/deps/ORB_SLAM2/src/FrameDrawer.cc" "/home/gapo/meng/deps/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/FrameDrawer.cc.o"
+  "/home/gapo/meng/deps/ORB_SLAM2/src/Initializer.cc" "/home/gapo/meng/deps/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Initializer.cc.o"
+  "/home/gapo/meng/deps/ORB_SLAM2/src/KeyFrame.cc" "/home/gapo/meng/deps/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/KeyFrame.cc.o"
+  "/home/gapo/meng/deps/ORB_SLAM2/src/KeyFrameDatabase.cc" "/home/gapo/meng/deps/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/KeyFrameDatabase.cc.o"
+  "/home/gapo/meng/deps/ORB_SLAM2/src/LocalMapping.cc" "/home/gapo/meng/deps/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/LocalMapping.cc.o"
+  "/home/gapo/meng/deps/ORB_SLAM2/src/LoopClosing.cc" "/home/gapo/meng/deps/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/LoopClosing.cc.o"
+  "/home/gapo/meng/deps/ORB_SLAM2/src/Map.cc" "/home/gapo/meng/deps/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Map.cc.o"
+  "/home/gapo/meng/deps/ORB_SLAM2/src/MapDrawer.cc" "/home/gapo/meng/deps/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/MapDrawer.cc.o"
+  "/home/gapo/meng/deps/ORB_SLAM2/src/MapPoint.cc" "/home/gapo/meng/deps/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/MapPoint.cc.o"
+  "/home/gapo/meng/deps/ORB_SLAM2/src/ORBextractor.cc" "/home/gapo/meng/deps/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/ORBextractor.cc.o"
+  "/home/gapo/meng/deps/ORB_SLAM2/src/ORBmatcher.cc" "/home/gapo/meng/deps/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/ORBmatcher.cc.o"
+  "/home/gapo/meng/deps/ORB_SLAM2/src/Optimizer.cc" "/home/gapo/meng/deps/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Optimizer.cc.o"
+  "/home/gapo/meng/deps/ORB_SLAM2/src/PnPsolver.cc" "/home/gapo/meng/deps/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/PnPsolver.cc.o"
+  "/home/gapo/meng/deps/ORB_SLAM2/src/Sim3Solver.cc" "/home/gapo/meng/deps/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Sim3Solver.cc.o"
+  "/home/gapo/meng/deps/ORB_SLAM2/src/System.cc" "/home/gapo/meng/deps/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/System.cc.o"
+  "/home/gapo/meng/deps/ORB_SLAM2/src/Tracking.cc" "/home/gapo/meng/deps/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Tracking.cc.o"
+  "/home/gapo/meng/deps/ORB_SLAM2/src/Viewer.cc" "/home/gapo/meng/deps/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Viewer.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  "../"
+  "../include"
+  "/usr/include/eigen3"
+  "/home/gapo/meng/deps/pangolin/include"
+  "/home/gapo/meng/deps/pangolin/build/src/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ deps/ORB_SLAM2/build/CMakeFiles/mono_kitti.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gapo/meng/deps/ORB_SLAM2/Examples/Monocular/mono_kitti.cc" "/home/gapo/meng/deps/ORB_SLAM2/build/CMakeFiles/mono_kitti.dir/Examples/Monocular/mono_kitti.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  "../"
+  "../include"
+  "/usr/include/eigen3"
+  "/home/gapo/meng/deps/pangolin/include"
+  "/home/gapo/meng/deps/pangolin/build/src/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/gapo/meng/deps/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
